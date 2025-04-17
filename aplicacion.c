7df27f75ee386d06e017c5386f06a1387ede20c1
augusto-dip <augustodip26@gmail.com>
2025-04-17 12:41:06 -0300

    terminada primera funcion

--- aplicacion.c
+++ aplicacion.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 struct compu
 {
     int velocidad;
@@ -9,14 +8,18 @@ struct compu
     int cantidad_nucleos;
     char *tipo_cpu;
 };
+void listarPCs(struct compu pcs[], int cantidad);
+void mostrarMasVieja(struct compu pcs[], int cantidad);
+void mostrarMasVeloz(struct compu pcs[], int cantidad);
 
 int main (){
+    int i;
     srand(time(NULL));
 
     char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"}; // Esto es un arreglo de 6 strings, y cada string puede tener hasta 9 caracteres
     struct compu pcs[5]; // Arreglo de 5 computadoras
 
-    for (int i = 0; i < 5; i++) {
+    for (i = 0; i < 5; i++) {
         pcs[i].velocidad = 1 + rand() % 3;               // entre 1 y 3
         pcs[i].anio = 2015 + rand() % (2024 - 2015 + 1); // entre 2015 y 2024
         pcs[i].cantidad_nucleos = 1 + rand() % 8;        // entre 1 y 8
@@ -24,8 +27,20 @@ int main (){
     }
 
     listarPCs(pcs, 5);
-    mostrarMasVieja(pcs, 5);
-    mostrarMasVeloz(pcs, 5);
+   // mostrarMasVieja(pcs, 5);
+  //  mostrarMasVeloz(pcs, 5);
 
     return 0;
+}
+// DECLARACION DE FUNCIONES //
+void listarPCs(struct compu pcs[], int cantidad){
+    int i;
+    for (i=0; i<cantidad; i++){
+        printf("PC %d:\n", i + 1);
+        printf("  Velocidad: %d GHz\n", pcs[i].velocidad);
+        printf("  Anio: %d\n", pcs[i].anio);
+        printf("  Nucleos: %d\n", pcs[i].cantidad_nucleos);
+        printf("  Tipo CPU: %s\n\n", pcs[i].tipo_cpu);
+        printf(" /////////////////////////////////////// \n");
+    }
 }
